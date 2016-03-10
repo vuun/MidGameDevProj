89bf16f9406d78ca616837b166e2fc8b6c1da28b
AppleJuicez <jinnawat202@hotmail.com>
2016-03-10 17:46:33 +0700

    Added Spawner and Fixed spawn range

---
+++ Assets/MeteorSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorSpawner : MonoBehaviour {
+    public GameObject met;
+    double delay = 1;
+	double sec = 0;
+	// Use this for initialization
+	void Start () {
+		Instantiate(met);
+	
+	}
+	
+	/*IEnumerator Example() 
+	{
+		Instantiate(met);
+        yield return new WaitForSeconds(1);
+    }*/
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Time.time > sec)
+		{
+			if(delay >0.1)
+			{
+				delay*=0.95;	
+			}
+			sec = sec+delay;
+			Instantiate(met);
+		}
+	}
+}
